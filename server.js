b4ec80cbb04432a15753a4effab774ab6b0b1d13
Sebastian Wiewiora <sbswwr+git@gmail.com>
2023-03-13 23:38:08 +0100

    set host

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ fastify.get('/', async (request, reply) => {
  * Run the server!
  */
 const start = async () => {
-  try {
-    await fastify.listen({ port: 3000 })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+  fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+  })
 }
 start()
